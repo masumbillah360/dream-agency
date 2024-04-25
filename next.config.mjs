7c7b5d62cbb8e0ce68fe303bf5ad81f987411cd5
masumbillah360 <masumbillah6584@gmail.com>
2024-04-25 11:42:59 +0600

    updated tailwind config and next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:[
-            "cdn.rareblocks.xyz"
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.rareblocks.xyz'
+      }
+    ],
+  },
 };
 
 export default nextConfig;
